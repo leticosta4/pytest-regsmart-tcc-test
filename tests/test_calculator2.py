c3ff3747d43f043ced2ab9ccd670fdb7f156d549
let <leticiacostaoa@gmail.com>
2026-08-29 23:05:43 -0300

    fix broken tests

--- tests/test_calculator2.py
+++ tests/test_calculator2.py
@@ -2,8 +2,8 @@ from calculator2 import mult, div
 
 
 def test_mult():
-    assert mult(2, 3) == 5
+    assert mult(2, 3) == 6
 
 
 def test_div():
-    assert div(5, 3) == 2
+    assert div(6, 3) == 2
